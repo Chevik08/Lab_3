f0bc091361e4cd3e794c6a1e94d5d14c8e984e0b
Chevik08 <76528394+Chevik08@users.noreply.github.com>
2021-03-22 17:02:05 +0700

    Lab_3.0.2

--- main.c
+++ main.c
@@ -1,42 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-
 #define BACKSPACE_KEY 8
-
+#define START_CHAR_RANGE 32
+#define END_CHAR_RANGE 1103
 #define START 1
 #define EXIT 2
-working = 1;
 
-process()
+int working = 1;
+
+void string()
 {
     setlocale(LC_ALL, "");
-    char* string = (char*)calloc(1, sizeof(char));
-    string[0] = '\0';
-    char symbol = 0;
-    int size = 1;
-
-    printf("Введите ваш текст: ");
-    while (symbol != '\n')
+    char* userStr = (char*)malloc(1 * sizeof(char));
+    userStr[0] = '\0';
+    char curChar = 0;
+    int curSize = 1;
+    printf("Enter your string: ");
+    while(curChar != '\n')
     {
-        symbol = getchar();
-        printf("%c", symbol);
-        fflush(stdout);
-    }
-}
+        curChar = getchar();
+        int deltaVal = 0;
+        int lengthDif = 0;
+        if (curChar == BACKSPACE_KEY)
+        {
+            deltaVal = -1;
+            lengthDif = 1;
+        }
+        else
+        {
+            if (curChar >= START_CHAR_RANGE && curChar <= END_CHAR_RANGE)
+            {
+            deltaVal = 1;
+            lengthDif = 2;
+            }
+            else
+                continue;
+        }
+        int newSize = curSize + deltaVal;
+        if (newSize == 0)
+            continue;
+        char* tmpStr = (char*)malloc(newSize * sizeof(char));
+        if (tmpStr)
+        {
+            for (int i = 0; i < newSize - lengthDif; ++i)
+                tmpStr[i] = userStr[i];
 
+            if (curChar != BACKSPACE_KEY)
+                tmpStr[newSize - 2] = curChar;
+            tmpStr[newSize - 1] = '\0';
+            free(userStr);
+            userStr = tmpStr;
+            curSize = newSize;
+        }
+        else
+        {
+            printf("Why?");
+            break;
+        }
+
+            printf("%c", curChar);
+    }
+    printf("\nEntered string: %s", userStr);
+    printf("\n");
+}
 
 int main()
 {
     int message = 0;
-    setlocale(LC_ALL, "");
-
+    char* userStr = (char*)malloc(1 * sizeof(char));
     while (working)
     {
         printf("===================================\n");
-        printf("Добро пожаловать в программу!\n");
-        printf("1. Запуск программы\n");
-        printf("2. Выход\n");
+        printf("%p\n", 'Рї');
+        printf("Welcome to the program!\n");
+        printf("1. Enter your string\n");
+        printf("2. Exit\n");
         printf("===================================\n");
 
         scanf("%d", &message);
@@ -44,27 +83,20 @@ int main()
         switch(message)
         {
         case START:
-            printf("Введите ваш текст:");
-            char symbol = 0;
-            char* string = (char*)calloc(1, sizeof(char));
-            string[0] = '\0';
-            int size = 1;
-
-            while (symbol != '\n')
-            {
-                symbol = getchar();
-                fflush(stdout);
-                printf("%c", symbol);
-            }
+            fflush(stdout);
+            fflush(stdin);
+            string(userStr);
             break;
         case EXIT:
-            printf("Выходим из програмыы\n");
+            printf("Quiting from the program\n");
+            free(userStr);
             working = 0;
             break;
         default:
             printf("Unusual Enter\n");
         }
     }
+
 
     return 0;
 }
